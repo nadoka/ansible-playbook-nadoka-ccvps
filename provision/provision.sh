310a40ad05e6b3623b6ee4da7ed048d6dc827fe1
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-07 17:08:19 +0900

    vagrant or production

--- provision/provision.sh
+++ provision/provision.sh
@@ -7,5 +7,10 @@ grep -q localhost /etc/ansible/hosts || cat >/etc/ansible/hosts <<EOF
 [local]
 localhost
 EOF
-[ -d /etc/ansible/roles ] || ansible-galaxy install -r /vagrant/provision/roles/Rolefile
-ansible-playbook /vagrant/provision/site.yml --connection=local
+if [ -d /vagrant ]; then
+    [ -d /etc/ansible/roles ] || ansible-galaxy install -r /vagrant/provision/roles/Rolefile
+    ansible-playbook /vagrant/provision/site.yml --connection=local
+else
+    [ -d provision/roles/znzj.ja_jp ] || ansible-galaxy install -f -p provision/roles -r provision/roles/Rolefile
+    ansible-playbook provision/site.yml --connection=local
+fi
