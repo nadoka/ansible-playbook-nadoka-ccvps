544a7f4812b27b062aab14084e3e9e9e35fffca7
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-07 17:25:29 +0900

    use sudo when needed

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 set -e
-apt-get install -y ansible
+if ! type ansible >/dev/null 2>&1; then
+    sudo apt-get install -y ansible
+fi
 # avoid "ERROR: provided hosts list is empty"
-grep -q localhost /etc/ansible/hosts || cat >/etc/ansible/hosts <<EOF
+if ! grep -q localhost /etc/ansible/hosts; then
+    sudo cat >/etc/ansible/hosts <<EOF
 
 [local]
 localhost
 EOF
+fi
 if [ -d /vagrant ]; then
     [ -d /etc/ansible/roles ] || ansible-galaxy install -r /vagrant/provision/roles/Rolefile
     ansible-playbook /vagrant/provision/site.yml --connection=local
 else
     [ -d provision/roles/znzj.ja_jp ] || ansible-galaxy install -f -p provision/roles -r provision/roles/Rolefile
-    ansible-playbook provision/site.yml --connection=local
+    sudo ansible-playbook provision/site.yml --connection=local
 fi
